c3124d5b4cc1b4fd2a691721cee6c02e8fbab008
QatarAndora <dimitrije.golubovic@borastankovic.edu.rs>
2022-12-28 19:29:45 +0100

    Dodat konstruktor i ispravljena klasa "Recept".

--- Model/Recept.cs
+++ Model/Recept.cs
@@ -7,9 +7,22 @@ using SQLite;
 
 namespace Friziderko.Model
 {
+    public struct Podatak
+    {
+        public Namirnica namirnica;
+        public int kolicina;
+    }
     [Table("Recept")]
     public class Recept
     {
+        public Recept(int id, string naziv, string opis, List<Podatak> podaci)
+        {
+            Id = id;
+            Naziv = naziv;
+            Opis = opis;
+            Podaci = podaci;
+        }
+
         [PrimaryKey, AutoIncrement, Column("_id")]
         public int Id { get; set; }
 
@@ -18,11 +31,8 @@ namespace Friziderko.Model
 
         [MaxLength(250)]
         public string Opis { get; set; }
-        public struct Podatak
-        {
-            public Namirnica namirnica;
-            public int kolicina;
-        }
+
+        public List<Podatak> Podaci = new List<Podatak>();
 
     }
 }
