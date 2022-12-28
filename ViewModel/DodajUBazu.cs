91565076c1924daf44abd6fd612be44dba529da1
Ika1009 <ilija.doncic@yahoo.com>
2022-12-28 19:10:56 +0100

    Dodao i za uzimanje namirnica funkciju

--- ViewModel/DodajUBazu.cs
+++ ViewModel/DodajUBazu.cs
@@ -10,38 +10,57 @@ namespace Friziderko.ViewModel
 {
     public class DodajUBazu
     {
-        private SQLiteConnection conn;
+        private SQLiteAsyncConnection conn;
         private string dbPath;
+        public DodajUBazu(string _dbPath) { dbPath = _dbPath; } // constructor da poveze put
+
         public void InitNamirnica()
         {
             if (conn != null)
                 return;
-            conn = new SQLiteConnection(dbPath);
-            conn.CreateTable<Namirnica>();
+            conn = new SQLiteAsyncConnection(dbPath);
+            conn.CreateTableAsync<Namirnica>();
         }
         public void InitRecept()
         {
             if (conn != null)
                 return;
-            conn = new SQLiteConnection(dbPath);
+            conn = new SQLiteAsyncConnection(dbPath);
             //conn.CreateTable<Namirnice>(); da se stavi za recept kad se napravi klasa
         }
         public void DodajNamirnicu(Namirnica namirnica)
         {
-            int result = 0;
             try
             {
-                // enter this line
                 InitNamirnica();
 
-                // basic validation to ensure a name was entered
+                // basic validation to ensure a namirnica is entered
                 if (namirnica is null)
-                    throw new Exception("Ne postojeca namirnica");
+                    throw new Exception("Nepostojeca namirnica");
 
-                // enter this line
-                result = conn.Insert(namirnica);
+                conn.InsertAsync(namirnica); // ubacuje u bazu
+
+                //StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            }
+            catch (Exception ex)
+            {
+                //StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
             }
         }
-        public DodajUBazu(string _dbPath) { dbPath = _dbPath; }
+        public async Task<List<Namirnica>> GetAllNamirniceAsync()
+        {
+            try
+            {
+                InitNamirnica();
+                return await conn.Table<Namirnica>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                //StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            return new List<Namirnica>();
+        }
+
     }
 }
