f83a0bc59118e2e0d0a5319cba5ca89ccbb7cb85
Nikola <pesicnikola50@gmail.com>
2022-12-30 18:33:55 +0100

    nista posebno

--- View/DodajNamirnicePopup.xaml.cs
+++ View/DodajNamirnicePopup.xaml.cs
@@ -10,14 +10,5 @@ public partial class DodajNamirnicePopup : Popup
 	{
 		InitializeComponent();
 	}
-	private void DodajElement(object sender, EventArgs e)
-	{
-		string Naziv = Nazivcina.Text;
-		string Opis = Opiscina.Text;
-		int Kolicina = int.Parse(Kolicincina.Text);
-		Namirnica namirnica = new Namirnica(Naziv, Opis, Kolicina, /*ovde treba da se stavi PUT DO SLIKE*/"0");
-		BazaPristupServis bazaPristupServis;
-		
-		//bazaPristupServis.DodajNamirnicu(namirnica);
-	}
+
 }
