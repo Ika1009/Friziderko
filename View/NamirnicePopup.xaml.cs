f8991dceef18fade9df7fe679dcf7c7985652adc
Nikola <pesicnikola50@gmail.com>
2023-01-10 22:04:41 +0100

    dodavanje slika(nije ulepsano)

--- View/NamirnicePopup.xaml.cs
+++ View/NamirnicePopup.xaml.cs
@@ -2,18 +2,19 @@ using CommunityToolkit.Maui.Views;
 using CommunityToolkit.Mvvm.Input;
 using Friziderko.Model;
 using Friziderko.ViewModel;
+using Microsoft.Win32;
 
 namespace Friziderko.View;
 
 public partial class NamirnicePopup : Popup
 {
     FriziderPageViewModel friziderPageViewModel;
+    FileResult result;
     public NamirnicePopup(FriziderPageViewModel vm)
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
         friziderPageViewModel = vm;
-        //Namirnica promena_teksta = new Namirnica();
-	}
+    }
 
 
     private async void DodajNamirnicu(object sender, EventArgs e)
@@ -21,13 +22,19 @@ public partial class NamirnicePopup : Popup
         string nazivNamirnice = NazivEntry.Text;
         string opisNamirnice = OpisEntry.Text;
         string kolicinaNamirnice = KolicinaEntry.Text;
+
         if (string.IsNullOrEmpty(nazivNamirnice) || string.IsNullOrEmpty(opisNamirnice) || string.IsNullOrEmpty(kolicinaNamirnice))
         {
             await Shell.Current.DisplayAlert("Greška", "Morate uneti sve vrednosti!", "OK");
             return;
         }
+        if (result == null)
+        {
+            await Shell.Current.DisplayAlert("Greška", "Unesite sliku!", "OK");
+            return;
+        }
 
-        Namirnica namirnica = new Namirnica(nazivNamirnice, opisNamirnice, int.Parse(kolicinaNamirnice), "Za sad nema");
+        Namirnica namirnica = new Namirnica(nazivNamirnice, opisNamirnice, int.Parse(kolicinaNamirnice), result.FullPath);
 
         await friziderPageViewModel.DodajNamirnicuAsync(namirnica);
 
@@ -37,4 +44,27 @@ public partial class NamirnicePopup : Popup
     {
         this.Close();
     }
+
+    private async void DodajSliku(object sender, EventArgs e)
+    {
+        //za upload slike
+        result = await FilePicker.PickAsync(new PickOptions
+        {
+            FileTypes = FilePickerFileType.Images, 
+            PickerTitle = "Izaberite sliku"
+        }); 
+
+        //ako se ne izabere slika
+        if(result == null)
+        {
+            return;
+        }
+
+
+
+        //var stream = await result.OpenReadAsync();
+
+        //Slika.Source =  ImageSource.FromStream(() => stream);
+
+	}
 }
