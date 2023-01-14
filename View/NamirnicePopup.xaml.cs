e0ca12832e64453078bcdc3d84c8c2f56098d173
Nikola <pesicnikola50@gmail.com>
2023-01-14 18:08:58 +0100

    slika nije obavezna

--- View/NamirnicePopup.xaml.cs
+++ View/NamirnicePopup.xaml.cs
@@ -23,27 +23,36 @@ public partial class NamirnicePopup : Popup
         string nazivNamirnice = NazivEntry.Text;
         string opisNamirnice = OpisEntry.Text;
         string kolicinaNamirnice = KolicinaEntry.Text;
+        Namirnica namirnica;
 
         if (string.IsNullOrEmpty(nazivNamirnice) || string.IsNullOrEmpty(opisNamirnice) || string.IsNullOrEmpty(kolicinaNamirnice))
         {
             await Shell.Current.DisplayAlert("Greška", "Morate uneti sve vrednosti!", "OK");
             return;
         }
+
+        // ako je resultat null tj ne izabere se slika, onda se doda znak pitanja umesto nje
         if (result == null)
         {
-            await Shell.Current.DisplayAlert("Greška", "Unesite sliku!", "OK");
-            return;
+            namirnica = new Namirnica(nazivNamirnice, opisNamirnice, int.Parse(kolicinaNamirnice), "help_icon.png");
         }
+        else
+        {
+			namirnica = new Namirnica(nazivNamirnice, opisNamirnice, int.Parse(kolicinaNamirnice), result.FullPath);
+		}
 
-        Namirnica namirnica = new Namirnica(nazivNamirnice, opisNamirnice, int.Parse(kolicinaNamirnice), result.FullPath);
+        
 
         await friziderPageViewModel.DodajNamirnicuAsync(namirnica);
 
+        //reset za sledece dodavanje
         NazivEntry.Text = "";
         OpisEntry.Text = "";
         KolicinaEntry.Text = "";
+        Slika.Source = "help_icon.png";
 
-        await friziderPageViewModel.GetNamirniceAsync(); // update kolekcije(jer se upravo dodala nova)
+
+		await friziderPageViewModel.GetNamirniceAsync(); // update kolekcije(jer se upravo dodala nova)
 
 
     }
